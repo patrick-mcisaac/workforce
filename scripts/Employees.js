5c37a1b9cfe729cd84e7090d3c12ecf9764a49af
Patrick-McIsaac <patrick.mcisaac.dev@gmail.com>
2025-08-08 13:06:11 -0500

    refactored html

--- scripts/Employees.js
+++ scripts/Employees.js
@@ -1,22 +1,28 @@
 // generate html to display employees
 export const Employees = async () => {
 	const response = await fetch(
-		" http://localhost:8088/employees?_expand=computer"
+		" http://localhost:8088/employees?_expand=computer&_expand=department"
 	)
 	const employees = await response.json()
 	let html = `
     <section class='employees'>
-        <h2>Employees</h2>
+        <h1>Employees</h1>
     `
 
 	const employeeHTML = employees
 		.map(employee => {
 			return `
-        <section class='employee-info'>
-            <p class='name'>${employee.firstName} ${employee.lastName}</p>
-            <p class='age'>${employee.age}</p>
-            <p class='employee-computer'>${employee.computer.model} ${employee.computer.year}</p>
-        </section>
+        <div class='employee'>
+            <header class='employee-name'>
+                <h2>${employee.firstName} ${employee.lastName}</h2>
+            </header>
+            <section class='employee-computer'>
+                <p>${employee.computer.model} ${employee.computer.year}</p>
+            </section>
+            <section class='employee-department'>
+                <p>Works in the ${employee.department.name}</p>
+            </section>
+        </div>
         `
 		})
 		.join("")
